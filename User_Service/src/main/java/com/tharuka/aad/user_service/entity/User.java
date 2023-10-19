505f98e3487e99271deb38c4706a772b36075690
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-19 09:38:04 +0530

    User entity create

--- User_Service/src/main/java/com/tharuka/aad/user_service/entity/User.java
+++ User_Service/src/main/java/com/tharuka/aad/user_service/entity/User.java
@@ -1,4 +1,26 @@
 package com.tharuka.aad.user_service.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_details")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String email;
+    private String address;
+    private String nicOrPassportNum;
+    private String userName;
+    private String password;
+    private String role;
+    private String proPicUrl;
+
 }
