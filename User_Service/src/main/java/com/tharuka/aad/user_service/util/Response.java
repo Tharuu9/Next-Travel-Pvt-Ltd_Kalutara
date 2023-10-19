7bd5d42992df78408e0934681f80ed7275941793
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-19 09:33:49 +0530

    First Commit

--- User_Service/src/main/java/com/tharuka/aad/user_service/util/Response.java
+++ User_Service/src/main/java/com/tharuka/aad/user_service/util/Response.java
@@ -1,4 +1,16 @@
 package com.tharuka.aad.user_service.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class Response {
+    private String status;
+    private String message;
+    private Object data;
 }
