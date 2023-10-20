e2be0adf929833315d89948049ec88ab6b7ee482
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-21 01:16:38 +0530

    reservation repo completed

--- Payment_Service/src/main/java/com/tharuka/aad/payment_service/repository/ReservationRepository.java
+++ Payment_Service/src/main/java/com/tharuka/aad/payment_service/repository/ReservationRepository.java
@@ -1,4 +1,13 @@
 package com.tharuka.aad.payment_service.repository;
 
-public interface ReservationRepository {
+import com.tharuka.aad.payment_service.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+    @Query("SELECT rr.reservedDate FROM Reservation rr WHERE rr.reservationId=:reservationId")
+    Date getReservedDateByReservationId(@Param("reservationId") String reservationId);
 }
