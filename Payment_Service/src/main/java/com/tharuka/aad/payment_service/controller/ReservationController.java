ec8b208758b7a27444bc15661ea9d91a661589ed
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-21 01:55:26 +0530

    put mapping completed

--- Payment_Service/src/main/java/com/tharuka/aad/payment_service/controller/ReservationController.java
+++ Payment_Service/src/main/java/com/tharuka/aad/payment_service/controller/ReservationController.java
@@ -5,10 +5,7 @@ import com.tharuka.aad.payment_service.service.ReservationService;
 import com.tharuka.aad.payment_service.util.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/reservation")
@@ -19,7 +16,18 @@ public class ReservationController {
 
     @PostMapping(value = "/make")
     public Response makeReservation(@RequestBody Reservation reservation){
-        return new Response(HttpStatus.OK,"Reserved..!",reservationService.makeReservation(reservation))
+        return new Response(HttpStatus.OK,"Reserved Successfully..!",reservationService.makeReservation(reservation));
+    }
+
+    @PutMapping(value = "/edit")
+    public Response editReservation(@RequestBody Reservation reservation){
+        if (findDateDifference(String.valueOf(reservation.getReservationId()))){
+            return new Response(HttpStatus.OK,"Edited..!",reservationService.editReservation(reservation));
+        }
+        return new Response(HttpStatus.SERVICE_UNAVAILABLE,"Can't Edited..!",null);
+    }
+
+    private boolean findDateDifference(String valueOf) {
     }
 
 }
