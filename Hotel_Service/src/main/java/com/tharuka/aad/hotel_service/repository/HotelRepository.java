c12b8915f156b77c4372c8d9af6fddaee8850b87
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-21 02:30:55 +0530

    hotel repo created

---
+++ Hotel_Service/src/main/java/com/tharuka/aad/hotel_service/repository/HotelRepository.java
@@ -0,0 +1,7 @@
+package com.tharuka.aad.hotel_service.repository;
+
+import com.tharuka.aad.hotel_service.entity.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HotelRepository extends JpaRepository<Hotel,Integer> {
+}
