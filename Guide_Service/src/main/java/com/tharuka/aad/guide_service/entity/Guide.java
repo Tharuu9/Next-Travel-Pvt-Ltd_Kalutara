6232614c91a111ca93569a392998a6acd9f781dc
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-20 23:32:54 +0530

    guide entity complete

--- Guide_Service/src/main/java/com/tharuka/aad/guide_service/entity/Guide.java
+++ Guide_Service/src/main/java/com/tharuka/aad/guide_service/entity/Guide.java
@@ -1,4 +1,19 @@
 package com.tharuka.aad.guide_service.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document("guide")
 public class Guide {
+
+    @Id
+    private String id;
+    private String name;
+    private String address;
 }
