93ac8a7bbdbc50390badc4e350d29c8803f27597
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-21 00:58:42 +0530

    reservation entity create

--- Payment_Service/src/main/java/com/tharuka/aad/payment_service/controller/ReservationController.java
+++ Payment_Service/src/main/java/com/tharuka/aad/payment_service/controller/ReservationController.java
@@ -1,4 +1,10 @@
 package com.tharuka.aad.payment_service.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/reservation")
 public class ReservationController {
+
 }
