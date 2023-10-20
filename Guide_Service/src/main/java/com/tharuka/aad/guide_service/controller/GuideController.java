38a908008c2e26b6acf4875b1eef07c40ce507a2
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-20 23:43:20 +0530

    guide service into guide controller

--- Guide_Service/src/main/java/com/tharuka/aad/guide_service/controller/GuideController.java
+++ Guide_Service/src/main/java/com/tharuka/aad/guide_service/controller/GuideController.java
@@ -1,5 +1,7 @@
 package com.tharuka.aad.guide_service.controller;
 
+import com.tharuka.aad.guide_service.service.GuideService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 @RequestMapping(value = "/guide")
 public class GuideController {
+
+    @Autowired
+    private GuideService guideService;
 }
