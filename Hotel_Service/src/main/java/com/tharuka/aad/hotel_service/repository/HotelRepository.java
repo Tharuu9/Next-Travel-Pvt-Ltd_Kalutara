81eb397d8b436e878af49f2f67cbc663500ce3b9
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-21 02:34:17 +0530

    reponse created

--- Hotel_Service/src/main/java/com/tharuka/aad/hotel_service/repository/HotelRepository.java
+++ Hotel_Service/src/main/java/com/tharuka/aad/hotel_service/repository/HotelRepository.java
@@ -4,4 +4,6 @@ import com.tharuka.aad.hotel_service.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HotelRepository extends JpaRepository<Hotel,Integer> {
+
+    boolean existsByIdAndName(Integer id, String name);
 }
---
+++ Hotel_Service/src/main/java/com/tharuka/aad/hotel_service/util/Response.java
@@ -0,0 +1,16 @@
+package com.tharuka.aad.hotel_service.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Response {
+    private String status;
+    private String message;
+    private Object data;
+}
