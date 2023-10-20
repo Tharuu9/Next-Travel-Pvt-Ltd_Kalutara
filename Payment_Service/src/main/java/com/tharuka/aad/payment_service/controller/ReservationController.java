8f72f9bb2233f381e4fe0f4a68ad8c33f5bb5d76
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-21 01:47:02 +0530

    post mapping create for reservation

--- Payment_Service/src/main/java/com/tharuka/aad/payment_service/controller/ReservationController.java
+++ Payment_Service/src/main/java/com/tharuka/aad/payment_service/controller/ReservationController.java
@@ -1,10 +1,25 @@
 package com.tharuka.aad.payment_service.controller;
 
+import com.tharuka.aad.payment_service.entity.Reservation;
+import com.tharuka.aad.payment_service.service.ReservationService;
+import com.tharuka.aad.payment_service.util.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/reservation")
 public class ReservationController {
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @PostMapping(value = "/make")
+    public Response makeReservation(@RequestBody Reservation reservation){
+        return new Response(HttpStatus.OK,"Reserved..!",reservationService.makeReservation(reservation))
+    }
 
 }
