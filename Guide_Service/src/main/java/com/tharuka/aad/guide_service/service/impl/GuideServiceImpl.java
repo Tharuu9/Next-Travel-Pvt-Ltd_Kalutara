1199c0c5df8d5ad053a8d70f4c8a481f73ad1a1e
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-20 23:40:23 +0530

    guide service impl update

--- Guide_Service/src/main/java/com/tharuka/aad/guide_service/service/impl/GuideServiceImpl.java
+++ Guide_Service/src/main/java/com/tharuka/aad/guide_service/service/impl/GuideServiceImpl.java
@@ -1,4 +1,28 @@
 package com.tharuka.aad.guide_service.service.impl;
 
-public class GuideServiceImpl {
+import com.tharuka.aad.guide_service.entity.Guide;
+import com.tharuka.aad.guide_service.repository.GuideRepository;
+import com.tharuka.aad.guide_service.service.GuideService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GuideServiceImpl implements GuideService {
+
+    @Autowired
+    private GuideRepository guideRepository;
+    @Override
+    public Guide addGuide(Guide guide) {
+        return guideRepository.save(guide);
+    }
+
+    @Override
+    public Guide updateGuide(Guide guide) {
+        return guideRepository.save(guide);
+    }
+
+    @Override
+    public void deleteGuide(String guideId) {
+        guideRepository.deleteById(guideId);
+    }
 }
