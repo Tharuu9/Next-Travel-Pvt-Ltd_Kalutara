1a943391e29278951b19a21fab461d9906bbbfb9
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-20 23:55:24 +0530

    rest template create

--- Common_Service/src/main/java/com/tharuka/aad/common_service/service/TravelPackageService.java
+++ Common_Service/src/main/java/com/tharuka/aad/common_service/service/TravelPackageService.java
@@ -1,12 +1,14 @@
 package com.tharuka.aad.common_service.service;
 
 import com.tharuka.aad.common_service.dto.TravelPackageDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 @Service
 public class TravelPackageService {
+    @Autowired
     private RestTemplate restTemplate;
 
     public List<TravelPackageDTO> getAllTravelPackages() {
