2ed7aa8618de4bd6e344e94b3860faeb752c90ac
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-20 22:15:35 +0530

    cors config update

--- Cloud_Gateway/src/main/java/com/tharuka/aad/cloud_gateway/CloudGatewayApplication.java
+++ Cloud_Gateway/src/main/java/com/tharuka/aad/cloud_gateway/CloudGatewayApplication.java
@@ -1,9 +1,12 @@
 package com.tharuka.aad.cloud_gateway;
 
+import com.tharuka.aad.cloud_gateway.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(CorsConfig.class)
 public class CloudGatewayApplication {
 
     public static void main(String[] args) {
