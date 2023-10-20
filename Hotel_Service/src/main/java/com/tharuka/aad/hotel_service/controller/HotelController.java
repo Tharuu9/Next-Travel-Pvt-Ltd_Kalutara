a3f28b7db72adbf77da52e83de283a4a028cfef3
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-21 02:52:45 +0530

    hotel controller update

--- Hotel_Service/src/main/java/com/tharuka/aad/hotel_service/controller/HotelController.java
+++ Hotel_Service/src/main/java/com/tharuka/aad/hotel_service/controller/HotelController.java
@@ -1,4 +1,22 @@
 package com.tharuka.aad.hotel_service.controller;
 
+import com.tharuka.aad.hotel_service.entity.Hotel;
+import com.tharuka.aad.hotel_service.service.HotelService;
+import com.tharuka.aad.hotel_service.util.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping(value = "/hotel")
 public class HotelController {
+
+    @Autowired
+    private HotelService hotelService;
+
+    @PostMapping("/add")
+    public Response addHotel(@RequestBody Hotel hotel){
+        hotelService.addHotel(hotel);
+        return new Response("Ok","Added..!",null);
+    }
 }
