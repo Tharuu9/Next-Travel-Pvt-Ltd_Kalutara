933844cb2cb97207ce0b487994c2488cc38d09e2
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-21 01:33:43 +0530

    reservation service completed

--- Payment_Service/src/main/java/com/tharuka/aad/payment_service/service/ReservationService.java
+++ Payment_Service/src/main/java/com/tharuka/aad/payment_service/service/ReservationService.java
@@ -1,4 +1,12 @@
 package com.tharuka.aad.payment_service.service;
 
+import com.tharuka.aad.payment_service.entity.Reservation;
+
+import java.util.Date;
+
 public interface ReservationService {
+    Reservation makeReservation(Reservation reservation);
+    Reservation editReservation(Reservation reservation);
+    Reservation cancelReservation(String reservationId);
+    Date getReservedDate(String  reservationId);
 }
---
+++ Payment_Service/src/main/java/com/tharuka/aad/payment_service/service/impl/ReservationServiceImpl.java
@@ -0,0 +1,4 @@
+package com.tharuka.aad.payment_service.service.impl;
+
+public class ReservationServiceImpl {
+}
