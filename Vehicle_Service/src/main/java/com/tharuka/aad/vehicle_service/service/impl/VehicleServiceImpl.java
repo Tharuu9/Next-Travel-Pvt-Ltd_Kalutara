4575c3b6e2fa1f0be2d44c8d7cca102fa64f76e3
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-22 08:40:42 +0530

    vehicle impl update

--- Vehicle_Service/src/main/java/com/tharuka/aad/vehicle_service/service/impl/VehicleServiceImpl.java
+++ Vehicle_Service/src/main/java/com/tharuka/aad/vehicle_service/service/impl/VehicleServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
 public class VehicleServiceImpl implements VehicleService {
     @Autowired
     private VehicleRepository vehicleRepo;
