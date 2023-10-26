9470f3f03f2e245986fb435cb57b7c3b8bc32fcf
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-26 21:20:05 +0530

    guide entity update

--- Guide_Service/src/main/java/com/tharuka/aad/guide_service/entity/Guide.java
+++ Guide_Service/src/main/java/com/tharuka/aad/guide_service/entity/Guide.java
@@ -1,6 +1,8 @@
 package com.tharuka.aad.guide_service.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
@@ -13,6 +15,7 @@ import lombok.*;
 public class Guide {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String name;
     private String gender;
