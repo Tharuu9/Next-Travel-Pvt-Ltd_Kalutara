b39a99b402ed6f570175e80ba33d056698aff307
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-26 08:06:44 +0530

    guide repo update

--- Guide_Service/src/main/java/com/tharuka/aad/guide_service/repository/GuideRepository.java
+++ Guide_Service/src/main/java/com/tharuka/aad/guide_service/repository/GuideRepository.java
@@ -1,9 +1,11 @@
 package com.tharuka.aad.guide_service.repository;
 
 import com.tharuka.aad.guide_service.entity.Guide;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.stereotype.Repository;
 
-@EnableMongoRepositories
-public interface GuideRepository extends MongoRepository<Guide, String> {
+@Repository
+public interface GuideRepository extends JpaRepository<Guide, String> {
 }
