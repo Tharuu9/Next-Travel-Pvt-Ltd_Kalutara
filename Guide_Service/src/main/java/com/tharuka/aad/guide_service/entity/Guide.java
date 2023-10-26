62b3c2ab425c56bbb95c5e8ee48d15da3ba636be
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-10-26 08:04:12 +0530

    guide entity update

--- Guide_Service/src/main/java/com/tharuka/aad/guide_service/entity/Guide.java
+++ Guide_Service/src/main/java/com/tharuka/aad/guide_service/entity/Guide.java
@@ -1,19 +1,19 @@
 package com.tharuka.aad.guide_service.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
 
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
-@Document("guide")
+@Getter
+@Setter
+
 public class Guide {
 
     @Id
-    private String id;
+    private Integer id;
     private String name;
     private String address;
 }
